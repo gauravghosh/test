84c7de4b88ffe27c5e10d0a399ad24e699479341
phoochka <phoochka@gmail.com>
2014-04-19 09:05:50 +0530

    updated test files

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ print i
 s = '''This is a multi line string.
 This is the second line.'''
 print s
+
+class MyClass:
+    def __init__(self, data):
+        self.d = data
+
+    def gived(self):
+         print self.d
+
+    def addStuff(self):
+         print 1+2
